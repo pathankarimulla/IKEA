f3da1b0c733edb31647ceeaea83b231f4806813e
pathankarimulla <43165948+pathankarimulla@users.noreply.github.com>
2018-09-13 11:23:23 +0530

    Rest curd operations
    Rest curd operations

---
+++ SpringBoot2RestServiceApplication/src/main/resources/data.sql
@@ -0,0 +1,8 @@
+
+create table student(id int(20),name varchar(20),passportid varchar(12));
+
+insert into student
+values(10001,'PATHAN', 'E1234567');
+
+insert into student
+values(10002,'KARIMULLA', 'A1234568');
